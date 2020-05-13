a98d2e001264374adff0dc3f45ea230a6df66733
alexandre schwarze <alexschwarze@live.fr>
2020-05-13 12:27:36 +0200

    Ajout classes Gestion menu et partie

---
+++ StarWars_TheCloneWars/StarWars/Managers/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StarWars.Managers
+{
+    public class GameManager
+    {
+        #region Attributes
+        #endregion
+
+        #region Ctor
+        #endregion
+
+        #region Methods
+        public void NewGame()
+        {
+
+        }
+
+        public void SearchSaveGame()
+        {
+
+        }
+
+        public void LoadGame()
+        {
+
+        }
+
+        public void SaveGame()
+        {
+
+        }
+
+        public void DeleteSavedGame()
+        {
+
+        }
+        #endregion
+    }
+}
---
+++ StarWars_TheCloneWars/StarWars/Managers/MenuManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StarWars.Managers
+{
+    public class MenuManager
+    {
+        #region Attributes
+        public String WelcomeText{ get; set; }
+        public String CharacterChoicText { get; set; }
+        public String GridSetUpText { get; set; }
+        public String CharDataText { get; set; }
+        #endregion
+
+        #region Ctor
+        #endregion
+
+        #region Methods
+        public void Welcome()
+        {
+
+        }
+
+        public void ChooseCharacter()
+        {
+
+        }
+
+        public void SetUpGrid()
+        {
+
+        }
+
+        public void CharacterStats()
+        {
+
+        }
+
+
+        #endregion
+    }
+}

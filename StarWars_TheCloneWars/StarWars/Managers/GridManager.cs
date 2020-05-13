b891c285601800486ae82672d652dcb08391355e
Maxime Vignet <ookhyteoo@gmail.com>
2020-05-13 12:23:03 +0200

    Ajout du GridManager et de la class Grid

---
+++ StarWars_TheCloneWars/StarWars/Managers/GridManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StarWars.Objects;
+
+namespace StarWars.Managers
+{
+	/// <summary>
+	/// Manager pour la création de la carte du jeu
+	/// </summary>
+	public class GridManager
+	{
+		#region Attributes
+		public Grid Grid
+		{
+			get; set;
+		}
+		#endregion
+
+		#region Ctor
+		
+		#endregion
+
+		#region Methods
+		/// <summary>
+		/// Method pour générer la grille du jeu
+		/// </summary>
+		/// <param name="index">Nombre de lignes et de colonnes de la grille de jeu</param>
+		/// <returns></returns>
+		public Grid GenereateGrid(int index)
+		{
+			Grid.Index = index;
+
+			string line = new string('_', index + 2);
+			string column = new string(' ', index);
+
+			Grid.lineWall = line;
+			Grid.columnWall = column;
+
+			return Grid;
+		}
+
+		/// <summary>
+		/// Method pour charger la grille du jeu
+		/// </summary>
+		/// <param name="grid">Grille à charger</param>
+		public void Load_Grid(Grid grid)
+		{
+
+		}
+		#endregion
+	}
+}
